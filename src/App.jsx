7453b78a910cf7e27949a1b8a0983bdfc861a2a2
Thayna de Oliveira Cavalcante <142625470+thacavalcannte@users.noreply.github.com>
2025-06-15 19:52:17 -0300

    feat: NavBar component
    NavBar com as seguintes ferramentas:
    - Logo (ainda será adicionado o svg)
    - barra de pesquisa
    - Projetos (vai redirecionar para a página -inicial- de projetos)
    - Sobre o site (leva para a página que fala sobre o trabalho)
    - Botão com "Olá, {adminName}!" (leva para às áreas de gerenciamento dos projetos, paginas e afins, exclusivas dos administradores)
    - Botão de Submissão de projetos "EXPOR PROJETO" 
    - Ícone de log-out

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import ProjectsPage from './pages/ProjectsPage.jsx'
-
+import NavBar from './components/NavBar/NavBar.jsx' 
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <ProjectsPage/>
-
+      <NavBar />
+  
     </>
   )
 }

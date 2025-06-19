312dc29a1fae97900db4a4c74b2b7151d7827f90
Laryssa Santos <142642145+LaryssaDD@users.noreply.github.com>
2025-06-19 19:26:00 -0300

    style: layout adjustments and new ProjectsPage.css
    - Filter.jsx: Ordenação das opções de filtro em ordem decrescente e alfabética
    - ProjectsCard.css: Alinhamento dos títulos dos cards para a esquerda e padronização do espaçamento entre os cards
    - ProjectsCard.jsx: Adicionado botão de excluir projeto
    - ProjectsPage.jsx: Adicionado botão de novo projeto
    - ProjectsPage.css: Criação de arquivo exclusivo para estilos da página de projetos
    - ProjectsPage.css: Ajustes de padding e alinhamento horizontal entre título, filtro e botão
    - App.css: Removidas regras desnecessárias e adicionadas @media queries para responsividade dos elementos da ProjectsPage
    - Correção de todos os erros de renderização

---
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import projects from "../../data/projects.json";
+import ProjectsList from "../../components/ProjectsList/ProjectsList.jsx";
+import Filter from "../../components/Filter/Filter.jsx"; 
+import "./ProjectsPage.css";
+import { BsPlusCircle } from "react-icons/bs";
+
+export default function ProjectsPage() {
+  const [filter, setFilter] = useState(null);
+
+  const filteredProjects = filter ? projects.filter((projeto) => {
+    if(filter.tipo == "Ano") return projeto.ano === filter.value;
+    if(filter.tipo == "Categoria") return projeto.tags.includes(filter.value);
+    if(filter.tipo == "Disciplina") return projeto.disciplina === filter.value;
+    if(filter.tipo == "Autor") return projeto.autor === filter.value;
+    return true;
+  })
+  : projects;
+
+  return (
+    <div className="projects-page-container">
+      <div className="projects-page-header">
+        <h1 className="projects-page-title">PROJETOS</h1>
+        <div className="projects-actions">
+          <button className="button-new-project"> <BsPlusCircle className="icon-button-new-project"/> Novo Projeto</button>
+          <Filter projects={projects} setFilter={setFilter} currentFilter={filter}/>
+        </div>
+      </div>
+        <ProjectsList projeto={filteredProjects}/>
+    </div>
+  );
+}

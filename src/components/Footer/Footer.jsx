09ce5847f4bcf0ac86e76660a0a08f592492c678
ESO Design <esodsign@gmail.com>
2025-07-02 21:45:28 -0300

    Footer com css atualizado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,23 @@
-//ajustar o nome das classes. por exemplo menu-footer
 import './footer.css';
 import { BsInstagram, BsWhatsapp, BsGeoAlt } from 'react-icons/bs';
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="logoAndCopyright">
-        <div className="logo"> {/* Logo */} </div>
-        <p className="copyright"> © 2025 Todos os direitos reservados. </p>
+      <div className="logo-and-copyright">
+        <div className="logo">{/* Logo */}</div>
+        <p className="copyright">© 2025 Todos os direitos reservados.</p>
       </div>
 
       <nav className="links">
-        <ul className="menuFooter">
+        <ul className="menu-footer">
           <li><a href="#digitalDesign">Digital Design</a></li>
           <li><a href="#projects">Projetos</a></li>
           <li><a href="#info">Informações</a></li>
           <li><a href="#aboutSite">Sobre o site</a></li>
         </ul>
 
-        <ul className="socialMedia">
+        <ul className="social-media">
           <li><BsGeoAlt size={24} /></li>
           <li><BsWhatsapp size={24} /></li>
           <li><BsInstagram size={24} /></li>
